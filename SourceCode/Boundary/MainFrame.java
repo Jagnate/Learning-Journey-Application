00b481ffd93090322d11ac5c27c5870c32bd07bf
Jin Si <79039778+Jagnate@users.noreply.github.com>
2023-03-21 14:59:46 +0800

    Add files via upload

---
+++ SourceCode/Boundary/MainFrame.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import javax.swing.plaf.FontUIResource;
+import java.awt.Font;
+import java.awt.event.*;
+
+
+public class MainFrame extends JFrame{
+    private JMenuBar menuBar = new JMenuBar();
+    private JMenu menu = new JMenu();
+
+    public MainFrame(){
+        super("Start with your Learning");
+    }
+
+    private void setMenu(){
+
+    }
+}
