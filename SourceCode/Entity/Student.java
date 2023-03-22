282530a779f368e9bd457476c824f4b07629847d
Jin Si <79039778+Jagnate@users.noreply.github.com>
2023-03-23 00:58:09 +0800

    Add Constructor and readInfor method
    Add Constructor and readInfor method
    Some tiny format has been changed.

--- SourceCode/Entity/Student.java
+++ SourceCode/Entity/Student.java
@@ -1,43 +1,77 @@
+package SourceCode.Entity;
+
+import java.io.*;
+
 public class Student {
-    private String stuID;             //The ID of student
-    private String stuPassWord;       //The password to log in our system
+
+    private static String database = "StudentInfo.txt";
+
+    private String stuID;             //The student's ID
+    private String stuPassWord;       //The password to log in student's journey
     private String stuMajor;          //The major of student
     private String stuSchool;         //The school of student
-    private String stuGrade;          //The grade of student
+    private int    stuYear;           //The year when student enter the college
+    
+    public Student(String ID, String PW){
+        readInfo(ID, PW);
+    }
+
+    public String getID()                  { return this.stuID; }
   
-    public String getID() {
-        return this.stuID;
-    }
-  
-    public void setID(String ID) {
-        this.stuID = ID;
-    }
+    public void   setID(String ID)         { this.stuID = ID; }
 
-    public String getPassWord() {
-        return this.stuPassWord;
-    }
+    public String getPassWord()            { return this.stuPassWord; }
  
-    public void setPassWord(String PW) {
-        this.stuPassWord = PW;
-    }
+    public void   setPassWord(String PW)   { this.stuPassWord = PW; }
 
-    public String getMajor() {
-        return this.stuMajor;
-    }
+    public String getMajor()               { return this.stuMajor;}
  
-    public void setMajor(String major) {
-        this.stuMajor = major;
-    }
+    public void   setMajor(String major)   { this.stuMajor = major; }
 
-    public String getSchool() {
-        return this.stuSchool;
-    }
+    public String getSchool()              { return this.stuSchool; }
  
-    public void setSchool(String school) {
-        this.stuSchool = school;
-    }
+    public void   setSchool(String school) { this.stuSchool = school; }
 
-    public String getGrade() {
-        return this.stuGrade;
-    }    
+    public int    getYear()                { return this.stuYear; }
+
+    public void   setYear(int year)        { this.stuYear = year; }
+
+    //Return value: -1 = Error; 0 = Success; 1 = Not Found the ID; 2=WrongPassword ;
+    public int readInfo(String ID, String PassWord){
+        try{
+            int resStatus = 1;
+            FileReader     fileReader     = new FileReader(database);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String         oneline        = bufferedReader.readLine();
+
+            //Read a line one by one
+            while(oneline!=null){
+                String[] oneStu = oneline.split(" ");
+                if(oneStu[0]==ID){
+                    if(oneStu[1]==PassWord){
+                        this.stuMajor  = oneStu[2];
+                        this.stuSchool = oneStu[3];
+                        this.stuYear   = Integer.parseInt(oneStu[4]);
+                        resStatus = 0;
+                        break;   
+                    }
+                    else{
+                        //To be updated with UI
+                        resStatus = 2;
+                        System.out.println("Wrong PassWord!");
+                    }
+                }
+                oneline = bufferedReader.readLine();
+            }
+
+            fileReader.close();
+            bufferedReader.close();
+            return resStatus;
+
+        }catch(IOException e){
+            //To be updated with UI
+            System.err.println(e);
+            return -1;
+        }
+    }
 }
