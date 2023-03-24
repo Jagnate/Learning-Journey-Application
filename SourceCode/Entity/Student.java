0948a9e62a6b68d218fd08ec59699f835e427fc6
Jin Si <79039778+Jagnate@users.noreply.github.com>
2023-03-24 21:10:13 +0800

    Delete the readInfo method

--- SourceCode/Entity/Student.java
+++ SourceCode/Entity/Student.java
@@ -1,20 +1,19 @@
 package SourceCode.Entity;
 
-import java.io.*;
+//import java.io.*;
 
 public class Student {
 
-    private static String database = "StudentInfo.txt";
+    public static String userLog  = "StudentLogin.txt";
+    public static String database = "StudentInfo.txt";
 
     private String stuID;             //The student's ID
     private String stuPassWord;       //The password to log in student's journey
+    private String stuName;
+    private int    stuBirth;
     private String stuMajor;          //The major of student
     private String stuSchool;         //The school of student
     private int    stuYear;           //The year when student enter the college
-    
-    public Student(String ID, String PW){
-        readInfo(ID, PW);
-    }
 
     public String getID()                  { return this.stuID; }
   
@@ -23,6 +22,14 @@ public class Student {
     public String getPassWord()            { return this.stuPassWord; }
  
     public void   setPassWord(String PW)   { this.stuPassWord = PW; }
+
+    public String getName()                { return this.stuName; }
+  
+    public void   setName(String name)     { this.stuName = name; }
+
+    public int    getBirth()               { return this.stuBirth; }
+  
+    public void   setBirth(int birth)      { this.stuBirth = birth; }
 
     public String getMajor()               { return this.stuMajor;}
  
@@ -36,44 +43,4 @@ public class Student {
 
     public void   setYear(int year)        { this.stuYear = year; }
 
-    //Return value: -1 = Error; 0 = Success; 1 = Not Found the ID; 2 = WrongPassword;
-    public int readInfo(String ID, String PassWord){
-        try{
-            int resStatus = 1;
-            FileReader     fileReader     = new FileReader(database);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-            String         oneline        = bufferedReader.readLine();
-
-            //Read a line one by one
-            while(oneline!=null){
-                //Data format: StuID StuPassWord stuMajor stuSchool stuYear
-                //         oneStu[0] oneStu[1]  oneStu[2] oneStu[3] oneStu[4]
-                String[] oneStu = oneline.split(" ");
-                if(oneStu[0]==ID){
-                    if(oneStu[1]==PassWord){
-                        this.stuMajor  = oneStu[2];
-                        this.stuSchool = oneStu[3];
-                        this.stuYear   = Integer.parseInt(oneStu[4]);
-                        resStatus = 0;
-                        break;   
-                    }
-                    else{
-                        //To be updated with UI
-                        resStatus = 2;
-                        System.out.println("Wrong PassWord!");
-                    }
-                }
-                oneline = bufferedReader.readLine();
-            }
-
-            fileReader.close();
-            bufferedReader.close();
-            return resStatus;
-
-        }catch(IOException e){
-            //To be updated with UI
-            System.err.println(e);
-            return -1;
-        }
-    }
 }
