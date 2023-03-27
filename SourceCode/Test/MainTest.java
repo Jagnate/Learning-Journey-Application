0e0cbe465dda1b1e7b135e45b9e4587eeb8938e1
Jin Si <79039778+Jagnate@users.noreply.github.com>
2023-03-27 23:38:50 +0800

    Add simple test for mainFrame and pass

--- SourceCode/Test/MainTest.java
+++ SourceCode/Test/MainTest.java
@@ -1,3 +1,13 @@
+package Test;
+
+import Boundary.MainFrame;
+
 public class MainTest {
-    
+    public static void main(String[] args) {
+        MainFrame mainFrame = new MainFrame();
+        mainFrame.setVisible(true);
+        // mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // mainFrame.setSize(400,400);
+        // mainFrame.setVisible(true);
+    }
 }
