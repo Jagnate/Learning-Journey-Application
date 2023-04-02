248bb203f01a1aa59db62a9723c5dc7a0710eb22
Jin Si <79039778+Jagnate@users.noreply.github.com>
2023-04-02 20:01:33 +0800

    [PASS]IterationOne Test

--- SourceCode/Test/IterationOneTest.java
+++ SourceCode/Test/IterationOneTest.java
@@ -3,6 +3,7 @@ package Test;
 import Boundary.LoginFrame;
 
 public class IterationOneTest {
+    //Test
     public static void main(String[] args) {
         LoginFrame startFrame = new LoginFrame();
         startFrame.setVisible(true);
