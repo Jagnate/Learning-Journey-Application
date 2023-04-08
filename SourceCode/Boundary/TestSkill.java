f719d5390e77f36edc569745e50c3dc56bb22d09
elleeeee-coder <89966173+elleeeee-coder@users.noreply.github.com>
2023-04-08 22:39:18 +0800

    Add files via upload

---
+++ SourceCode/Boundary/TestSkill.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+/**
+ * @version 2.1
+ * @auther ZhangDan, Liu Yuxuan
+ * @data 2023/4/8 15:06
+ */
+public class TestSkill {
+    public static void main(String[] args) {
+        Skill frame = new Skill();
+        frame.init("MySkills");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
