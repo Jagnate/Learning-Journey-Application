8831575607e446f549c63972d4bb63fdf54dfa17
ajnnjaa <128280818+ajnnjaa@users.noreply.github.com>
2023-04-10 19:31:22 +0800

    create Course class
    create Course class

---
+++ SourceCode/Entity/Course.java
@@ -0,0 +1,69 @@
+import java.util.ArrayList;
+
+public class Course {
+    
+    private ArrayList<Skill> skillList = new ArrayList<Skill>();
+    private String  courseName;
+    private int     credit;
+    private int     GPA;
+    private boolean completed; 
+    private boolean pass;
+    private boolean courseType; //true compuslory; false optional
+    private int i;
+
+    public Skill getSkill(){
+        return skillList.get(i);
+    }
+
+    public void setSkill(Skill newSkill){
+        skillList.add(newSkill);
+    }
+
+    public String getCourseName() {
+        return this.courseName;
+    }
+  
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public int getCredit() {
+        return this.credit;
+    }
+ 
+    public void setCredit(int credit) {
+        this.credit = credit;
+    }
+
+    public int getGPA() {
+        return this.GPA;
+    }
+ 
+    public void setGPA(int GPA) {
+        this.GPA = GPA;
+    }
+
+    public boolean getCompleted() {
+        return this.completed;
+    }
+ 
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public boolean getPass() {
+        return this.pass;
+    }
+ 
+    public void setPass(boolean pass) {
+        this.pass = pass;
+    }
+
+    public boolean getCourseType() {
+        return this.courseType;
+    }
+ 
+    public void setCourseType(boolean courseType) {
+        this.courseType = courseType;
+    }
+}
