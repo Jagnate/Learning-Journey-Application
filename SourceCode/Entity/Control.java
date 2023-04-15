21ad54e79a3f4a8220156c126650afb2fe6659c3
出租先生 <104835036+fishingz@users.noreply.github.com>
2023-04-15 18:30:07 +0800

    Update Control.java

--- SourceCode/Entity/Control.java
+++ SourceCode/Entity/Control.java
@@ -7,7 +7,7 @@ public class Control{
     public ArrayList<Course> course1 = new ArrayList<Course>();  //主函数
     Course course2 = new Course();
 
-    public void readInfo(){
+    public void readInfo(String ID, int year){
         try{
 
             FileReader     fileReader     = new FileReader("Courses.txt");
@@ -16,24 +16,87 @@ public class Control{
 
             //Read a line one by one
             while(oneline!=null){
+
                 String[] oneInfo = oneline.split(" ");
+
                 course2.setIndex(Integer.parseInt(oneInfo[0]));
                 course2.setCourseName(oneInfo[1]);
                 course2.setCredit(Integer.parseInt(oneInfo[2]));
                 course2.setCourseType(Boolean.valueOf(oneInfo[3]));
                 course2.setYear(Integer.parseInt(oneInfo[4]));
-                String[] oneInfo2 = oneInfo[5].split(".");
 
+                String[] oneInfo2 = oneInfo[5].split(".");
                 int len = oneInfo2.length;
                 for (int i = 0; i < len; i++) {
                     course2.skillList.add(Integer.parseInt(oneInfo2[i]));
                 }
+
                 course1.add(course2);
                 oneline = bufferedReader.readLine();
             }
 
             fileReader.close();
             bufferedReader.close();
+
+            fileReader     = new FileReader("StuCourse.txt");
+            bufferedReader = new BufferedReader(fileReader);
+            oneline        = bufferedReader.readLine();
+
+            //Read a line one by one
+            while(oneline!=null){
+
+                String[] oneInfo = oneline.split(" ");
+
+                if(oneInfo[0].equals(ID)){
+
+                    String[] oneInfo2 = oneInfo[1].split(".");
+                    int len = oneInfo2.length;
+                    for (int i = 0; i < len; i++) {
+                             course1.get(Integer.parseInt(oneInfo2[i])).setTolearn(true);
+                    }
+                    
+                }
+                oneline = bufferedReader.readLine();
+            }
+
+            fileReader.close();
+            bufferedReader.close();
+
+
+
+            fileReader     = new FileReader("StuGPA.txt");
+            bufferedReader = new BufferedReader(fileReader);
+            oneline        = bufferedReader.readLine();
+
+            //Read a line one by one
+            while(oneline!=null){
+
+                String[] oneInfo = oneline.split(" ");
+
+                if(oneInfo[0].equals(ID)){
+                    course1.get(Integer.parseInt(oneInfo[1])).setGPA(Integer.parseInt(oneInfo[2]));
+                    course1.get(Integer.parseInt(oneInfo[1])).setCompleted(true);
+                    if(Integer.parseInt(oneInfo[2])<60){
+                        course1.get(Integer.parseInt(oneInfo[1])).setPass(false);
+                    }
+                    else{
+                        course1.get(Integer.parseInt(oneInfo[1])).setPass(true);
+                    }
+                }
+                oneline = bufferedReader.readLine();
+            }
+
+            fileReader.close();
+            bufferedReader.close();
+
+            for (Course i : course1) {
+                if(i.getYear()<year-2020 && i.getTolearn()==true){
+                    i.setCompleted(false);
+                }
+                if(i.getYear()>=year-2020 && i.getTolearn()==true){
+                    i.setCompleted(true);
+                }
+            }
 
         }catch(IOException e){
             //To be updated with UI
@@ -42,8 +105,8 @@ public class Control{
         }
     }
 
-    public static void main(String[] args) {
+   /* public static void main(String[] args) {
        Control test = new Control();
-       test.readInfo();
-    }
+       test.readInfo("aaaa",2022);
+    }*/
 }
