769a55b99c30cee3de2b2aa4ac8783b50cf00b37
Sophia-Dan <89966054+Sophia-Dan@users.noreply.github.com>
2023-05-07 20:23:53 +0800

    Add files via upload

---
+++ SourceCode/Boundary/AchTest.java
@@ -0,0 +1,12 @@
+package Ach;
+
+public class AchTest {
+    public static void main(String[] args) {
+    AchFrame frame = new AchFrame();
+
+    frame.init("MyAchievement");
+    frame.setVisible(true);
+    // frame.setSize(500, 500);
+    frame.setResizable(false);
+    }
+}
