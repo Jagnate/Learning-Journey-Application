74571c44d025f8c10e71c9db17e256ec853f6bee
出租先生 <104835036+fishingz@users.noreply.github.com>
2023-05-23 15:19:59 +0800

    Update CourseControl.java

--- SourceCode/Control/CourseControl.java
+++ SourceCode/Control/CourseControl.java
@@ -1,10 +1,12 @@
 package Control;
 
+import Entity.Course;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.io.*;
-      
-import Entity.Course;
 
 
 public class CourseControl{
@@ -12,10 +14,30 @@ public class CourseControl{
     public ArrayList<Course>  courselist  = new ArrayList<Course>();
     public ArrayList<Integer> courseIndex = new ArrayList<Integer>();
 
+    public int[] sumGPA= new int[5];
+    public int[] coursecnt= new int[5];
+    public int[] GPA= new int[5];
+
+    public class maxGPA{
+        private String             courseName;
+        private int                GPA;
+
+       /* public String  getCourseName()                  { return this.courseName; }
+
+        public void    setCourseName(String courseName) { this.courseName = courseName; }
+
+        public int     getGPA()                         { return this.GPA; }
+
+        public void    setGPA(int GPA)                  { this.GPA = GPA; }*/
+    }
+    
+    public maxGPA[] maxGPA= new maxGPA[3];
+
     public CourseControl(String ID, int year){
         this.readCourses(ID);
         this.readGeneralInfo();
         this.readPersonInfo(ID,year);
+
     }
 
     public void readCourses(String ID){
@@ -100,6 +122,19 @@ public class CourseControl{
                             for(int j=0;j<courselist.size();j++){
                                 if(courselist.get(j).getIndex()==courseIndex.get(i)){
                                     courselist.get(j).setGPA(Integer.parseInt(oneInfo[2]));
+                                    
+                                    for(int k=1;k<=3;k++){
+                                        if(courselist.get(j).getGPA()>=maxGPA[i].GPA){
+                                            for(int l=3;l<=i+1;l--){
+                                                maxGPA[l].courseName=maxGPA[l+1].courseName;
+                                                maxGPA[l].GPA=maxGPA[l-1].GPA;                                            
+                                            }
+                                            maxGPA[k].GPA=courselist.get(j).getGPA();
+                                            maxGPA[k].courseName=courselist.get(j).getCourseName();
+                                        }
+                                    }
+                                    coursecnt[courselist.get(j).getYear()]+=courselist.get(j).getGPA();
+                                    sumGPA[courselist.get(j).getYear()]+=courselist.get(j).getGPA()*courselist.get(j).getCredit();
                                     if(Integer.parseInt(oneInfo[2])<60){
                                         courselist.get(j).setPass(false);
                                     }else{
@@ -118,8 +153,14 @@ public class CourseControl{
                         }
                     }
                 }
+
                 oneline = bufferedReader.readLine();
             }
+
+            for(int i=1;i<=4;i++){
+                GPA[i]=sumGPA[i]/coursecnt[i];
+            }
+
 
             fileReader.close();
             bufferedReader.close();
